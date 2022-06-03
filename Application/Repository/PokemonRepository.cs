0c1ceb9ff15f879c197dcb956964a8f8341126d5
Javier Perez <javierpdc13@hotmail.com>
2022-06-03 19:19:06 -0400

    Add Application Layer

---
+++ Application/Repository/PokemonRepository.cs
@@ -0,0 +1,19 @@
+﻿using Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Repository
+{
+    public class PokemonRepository
+    {
+        private readonly ApplicationContext _dbContext;
+
+        public PokemonRepository(ApplicationContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
