8616f6eee41bd818f938d8c8f9b00e82dd2b436e
Javier Perez <javierpdc13@hotmail.com>
2022-06-03 11:14:41 -0400

    Add Database Layer

---
+++ Database/Models/Pokemon.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.Models
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImgUrl { get; set; }
+
+        public int PrimaryTypeId { get; set; }
+        public int SecondaryTypeId { get; set; }
+        public int RegionId { get; set; }
+
+        //Navigation property
+        public PokemonType PrimaryType { get; set; }
+        public PokemonType SecondaryType { get; set; }
+        public Region Region { get; set; }
+    }
+}
---
+++ Database/Models/PokemonType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.Models
+{
+    public class PokemonType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        //Navigation property
+        public ICollection<Pokemon> Pokemon { get; set; }
+    }
+}
