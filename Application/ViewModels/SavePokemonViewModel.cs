27c5ddbc1cdda6467b53dd4c2526e0dde8ac1e6c
Javier Perez <javierpdc13@hotmail.com>
2022-06-04 19:30:48 -0400

    Add validations to pokemon model props

--- Application/ViewModels/SavePokemonViewModel.cs
+++ Application/ViewModels/SavePokemonViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +10,14 @@ namespace Application.ViewModels
     public class SavePokemonViewModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "The name of this Pokémon is required")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "The image of this Pokémon is required")]
         public string ImgUrl { get; set; }
+        [Required(ErrorMessage = "The type of this Pokémon is required")]
         public int PrimaryTypeId { get; set; }
         public int SecondaryTypeId { get; set; }
+        [Required(ErrorMessage = "The region of this Pokémon is required")]
         public int RegionId { get; set; }
     }
 }
