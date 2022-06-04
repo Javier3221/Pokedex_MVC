701fed31d1f9c6da408af59c466c97a44c4d4c56
Javier Perez <javierpdc13@hotmail.com>
2022-06-04 12:41:20 -0400

    Add home controller and index page

---
+++ Pokédex_MVC/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Application.Services;
+using Database;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pokédex_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly PokemonService _pokemonService;
+
+        public HomeController(ApplicationContext dbContext)
+        {
+            _pokemonService = new(dbContext);
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _pokemonService.GetAllViewModel());
+        }
+    }
+}
