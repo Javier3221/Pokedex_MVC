f4f4a0c7afff47304168f030ebef711437590c47
Javier Perez <javierpdc13@hotmail.com>
2022-06-04 12:41:45 -0400

    Create view model for saving pokemon

---
+++ Application/ViewModels/SavePokemonViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.ViewModels
+{
+    public class SavePokemonViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImgUrl { get; set; }
+        public int PrimaryTypeId { get; set; }
+        public int SecondaryTypeId { get; set; }
+        public int RegionId { get; set; }
+    }
+}
