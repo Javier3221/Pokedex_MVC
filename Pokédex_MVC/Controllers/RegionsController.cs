bc02ae560fc8ae3a6bea2c5520e7692d1def4ab8
Javier Perez <javierpdc13@hotmail.com>
2022-06-05 10:34:28 -0400

    Add main page action on regions controller

--- Pokédex_MVC/Controllers/RegionsController.cs
+++ Pokédex_MVC/Controllers/RegionsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Application.Repositories;
+using Database;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,16 @@ namespace Pokédex_MVC.Controllers
 {
     public class RegionsController : Controller
     {
-        public IActionResult Index()
+        private readonly RegionRepository _regionRepository;
+
+        public RegionsController(ApplicationContext dbContext)
         {
-            return View();
+            _regionRepository = new(dbContext);
+        }
+
+        public async Task<IActionResult> RegionList()
+        {
+            return View(await _regionRepository.GetAllAsync());
         }
     }
 }
