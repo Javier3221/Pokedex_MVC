24cdbd9ba4e3ad0d0ebb60a71bea4fe514b65d88
Javier Perez <javierpdc13@hotmail.com>
2022-06-05 13:44:37 -0400

    Create TypeViewModel

---
+++ Application/ViewModels/TypeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.ViewModels
+{
+    public class TypeViewModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "The Type name is required")]
+        public string Name { get; set; }
+    }
+}
