4253cb5fbcd6626f29673e74dc83807526845fcc
Javier Perez <javierpdc13@hotmail.com>
2022-06-05 13:51:36 -0400

    Add CRUD actions to type controller

--- Pokédex_MVC/Controllers/TypesController.cs
+++ Pokédex_MVC/Controllers/TypesController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Database;
+using Application.ViewModels;
 
 namespace Pokédex_MVC.Controllers
 {
@@ -19,6 +20,46 @@ namespace Pokédex_MVC.Controllers
         public async Task<IActionResult> TypeList()
         {
             return View(await _typeService.GetAllViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(TypeViewModel vm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("SaveType", vm);
+            }
+
+            await _typeService.Add(vm);
+            return RedirectToRoute(new { controller = "Types", action = "TypeList" });
+        }
+
+        public IActionResult Create()
+        {
+            return View("SaveType", new TypeViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(TypeViewModel vm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("SaveType", vm);
+            }
+
+            await _typeService.Update(vm);
+            return RedirectToRoute(new { controller = "Types", action = "TypeList" });
+        }
+
+        public async Task<IActionResult> Update(int id)
+        {
+            return View("SaveType", await _typeService.GetByIdViewModel(id));
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _typeService.Delete(id);
+            return RedirectToRoute(new { controller = "Types", action = "TypeList" });
         }
     }
 }
