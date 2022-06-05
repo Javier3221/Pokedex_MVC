ce778d38ed15d8ccc8636a749476a58c8f0fbddb
Javier Perez <javierpdc13@hotmail.com>
2022-06-05 11:10:29 -0400

    Add name validation for regions

--- Application/ViewModels/RegionViewModel.cs
+++ Application/ViewModels/RegionViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace Application.ViewModels
     public class RegionViewModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "The region name is required")]
         public string Name { get; set; }
     }
 }
