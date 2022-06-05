104f7e6e0779af80cf14a0d305cc641fafbffb75
Javier Perez <javierpdc13@hotmail.com>
2022-06-05 13:47:02 -0400

    Add typelist action to controller

--- Pokédex_MVC/Controllers/TypesController.cs
+++ Pokédex_MVC/Controllers/TypesController.cs
@@ -1,16 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Application.Services;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Database;
 
 namespace Pokédex_MVC.Controllers
 {
     public class TypesController : Controller
     {
-        public IActionResult Index()
+        private readonly TypeService _typeService;
+
+        public TypesController(ApplicationContext dbContext)
         {
-            return View();
+            _typeService = new(dbContext);
+        }
+        public async Task<IActionResult> TypeList()
+        {
+            return View(await _typeService.GetAllViewModel());
         }
     }
 }
